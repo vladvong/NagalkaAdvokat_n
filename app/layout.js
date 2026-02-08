2f221fc3cee5ed143d148bf0a29886e22787b534
transport <>
2026-02-08 02:57:46 +0100

    new

--- app/layout.js
+++ app/layout.js
@@ -73,6 +73,13 @@ gtag('js', new Date());
 
 gtag('config', 'G-VG4Z8YLFH3');`}
         </Script>
+        <Script id="binotel-widget" strategy="afterInteractive">{
+          `(function(d, w, s) {
+  var widgetHash = '0g6n8j4sex72h4vne9ju', gcw = d.createElement(s); gcw.type = 'text/javascript'; gcw.async = true;
+  gcw.src = 'https://widgets.binotel.com/getcall/widgets/' + widgetHash + '.js';
+  var sn = d.getElementsByTagName(s)[0]; sn.parentNode.insertBefore(gcw, sn);
+})(document, window, 'script');`
+        }</Script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <LanguageProvider>
