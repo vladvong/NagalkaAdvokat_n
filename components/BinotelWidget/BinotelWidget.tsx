00f9151ec5fd4e8a046fa2d8e6abd6da17edce5e
transport <>
2026-02-08 03:39:23 +0100

    new

--- components/BinotelWidget/BinotelWidget.tsx
+++ components/BinotelWidget/BinotelWidget.tsx
@@ -3,19 +3,10 @@ import Script from "next/script";
 export default function BinotelWidget() {
   return (
     <>
-      <div id="binotelGetCall"></div>
+      <div id="binotelGetCall" />
       <Script
+        src="https://widgets.binotel.com/getcall/widgets/0g6n8j4sex72h4vne9ju.js"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(d, w, s) {
-  var widgetHash = '0g6n8j4sex72h4vne9ju', gcw = d.createElement(s);
-  gcw.type = 'text/javascript';
-  gcw.async = true;
-  gcw.src = '//widgets.binotel.com/getcall/widgets/' + widgetHash + '.js';
-  var sn = d.getElementsByTagName(s)[0];
-  sn.parentNode.insertBefore(gcw, sn);
-})(document, window, 'script');`,
-        }}
       />
     </>
   );
